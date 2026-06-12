60d1bb9745727588b90b860e1161f00fc6e9a83c
MarketingTeam12 <marketing@honeytranslations.com>
2026-06-12 14:45:03 +0530

    Add Google Tag Manager script

--- src/components/SeoHead.tsx
+++ src/components/SeoHead.tsx
@@ -187,6 +187,20 @@ export function SeoHead({
         gtag('config', 'G-F62WCKN2YQ');
       `}
     </Script>
+
+    <Script id="gtm" strategy="afterInteractive">
+    {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];
+      w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),
+      dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;
+      j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MP3J3QNH');
+    `}
+    </Script>
   </>
   );
 }
